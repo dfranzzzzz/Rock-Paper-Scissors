3ea6cee2a322e34cca39326c687437f5964020d2
Franz Decinal <dfranzzzzz05@gmail.com>
2021-02-05 10:47:07 +0800

    update
    changed integer outputs to string equivalents

--- main.js
+++ main.js
@@ -1,40 +1,44 @@
-picking();
-// score();
+round();
 
-function picking() {
-  let userPick = prompt("Pick your Hero(warrior|archer|mage)");
+function computerPick() {
   let compPick = Math.floor(Math.random() * (3) + 1);
 
-  if (userPick == 'warrior') {
+  return compPick;
+}
+
+function round() {
+  let userHero = prompt("Pick your hero(warrior | archer | mage)")
+
+  if (userHero == 'warrior') {
     userPick = 1;
-  } else if (userPick == 'archer') {
+  } else if (userHero == 'archer') {
     userPick = 2;
-  } else if (userPick == 'mage') {
+  } else if (userHero == 'mage') {
     userPick = 3;
   }
-  console.log(userPick);
-  console.log(compPick);
-  decision (userPick, compPick);
-}
 
+  let compPick = computerPick();
 
-
-function decision(userHero, compHero) {
-  winner = (((userHero - compHero) % 3) + 3) % 3;
-
-  if (winner === 2) {
-    console.log("user wins");
-  } else if (winner === 1) {
-    console.log("comp wins");
-  } else if (winner === 0) {
-    console.log("tie");
+  if (compPick === 1) {
+    compHero = 'warrior';
+  } else if (compPick === 2) {
+    compHero = 'archer';
+  } else if (compPick === 3) {
+    compHero = 'mage';
   }
 
+  decision = (((userPick - compPick % 3) + 3) % 3);
+
+  if (decision === 2) {
+    message = `You win! A ${userHero} beats a ${compHero}!`;
+  } else if (decision === 1) {
+    message = `You lose! A ${compHero} beats a ${userHero}..`;
+  } else if (decision === 0) {
+    message = `It's a tie!`;
+  }
+
+  console.log(userHero);
+  console.log(compHero);
+
+  console.log(message);
 }
-
-
-
-
-
-
-
